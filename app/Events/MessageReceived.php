d1ef95229a6ef6e9506a9267f7f0244d3a1c29aa
moutiasaad <moutiasaad481@gmail.com>
2026-05-22 23:08:07 +0100

    feat: Update broadcasting events to use ShouldBroadcastNow for improved real-time functionality

--- app/Events/MessageReceived.php
+++ app/Events/MessageReceived.php
@@ -5,7 +5,7 @@ namespace App\Events;
 use App\Models\Message;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
